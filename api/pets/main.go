46e9acf3869a30c2ffefc957de2dd31ac20e7b73
mick-roper <mick@theropers.co.uk>
2021-03-21 09:53:07 +0000

    added a pets api

---
+++ api/pets/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/pets", handlerFunc)
+
+	server := http.Server{
+		Handler:      r,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
+
+func handlerFunc(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	default:
+		{
+			res.WriteHeader(501)
+			res.Write([]byte("not implemented"))
+		}
+	}
+}
